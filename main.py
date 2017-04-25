dd12de3319437dec37f216eb55868fabf5be0a66
mkarmowski <mateusz@karmowski.com>
2017-04-25 18:48:54 +0200

    main updated with like

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from modules.like import get_links_by_tag, like_post
 from modules.login import login_user
 from webdriver.options import chrome_options
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 
 class InstaMain:
@@ -20,3 +22,46 @@ class InstaMain:
         else:
             print('Not logged in. Wrong user data.')
             self.logs.write('Not logged in. Wrong user data.\n')
+
+        return self
+
+    def like_image(self, tags=None, count=100):
+        liked_images = 0
+        already_liked = 0
+        liked_links = []
+        already_liked_links = []
+
+        for index, tag in enumerate(tags):
+            print(index), print(tag)
+            print('Current tag: {} ({}/{})\n'.format(tag, index + 1, len(tags)))
+            self.logs.write('Tag: {} ({}/{})\n'.format(tag, index + 1, len(tags)))
+
+            links = get_links_by_tag(self.browser, tag, count)
+
+            for ind, link in enumerate(links):
+                self.logs.write('{}/{}: {}\n'.format(ind + 1, len(links), link))
+                print('{}/{}\n'.format(ind + 1, len(links)))
+                self.browser.get(link)
+
+                try:
+                    liked = like_post(self.browser)
+
+                    if liked:
+                        liked_images += 1
+                        liked_links.append(link)
+                    else:
+                        already_liked += 1
+                        already_liked_links.append(link)
+
+                except NoSuchElementException:
+                    self.logs.write('Invalid Page: {}\n'.format(NoSuchElementException))
+                    print('Invalid Page: {}\n'.format(NoSuchElementException))
+
+        self.logs.write('Liked images: {}\n'.format(liked_images))
+        self.logs.write('Already liked images: {}\n'.format(already_liked))
+        self.logs.write('Links to liked images: {}\n'.format(liked_links))
+        self.logs.write('Links to already liked images: {}\n'.format(already_liked_links))
+        print('Liked images:\n{}\n'.format(liked_images))
+        print('Already liked images:\n{}\n'.format(already_liked))
+
+        return self
--- modules/like.py
+++ modules/like.py
@@ -5,10 +5,11 @@ from selenium.webdriver.common.keys import Keys
 
 
 def get_links_by_tag(browser, tag, count):
+    """Likes posts by specified tag. Tags must be passed as list"""
+
     browser.get('https://www.instagram.com/explore/tags/' + (tag[1:] if tag[:1] == '#' else tag))
 
     body = browser.find_element_by_tag_name('body')
-
     stop = True
     try:
         load_more = body.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/a')
@@ -30,17 +31,17 @@ def get_links_by_tag(browser, tag, count):
         count_left = count - links_count
         new_page_needed = ceil(count_left / 12)
 
-    for i in range(new_page_needed):
-        starting_count = links_count
-        body.send_keys(Keys.END)
-        sleep(1)
-        body.send_keys(Keys.HOME)
-        sleep(1)
-        link_tags = main_tag.find_elements_by_tag_name('a')
-        links_count = len(link_tags)
-        stop = (starting_count == links_count)
-        if stop:
-            break
+        for i in range(new_page_needed):
+            starting_count = links_count
+            body.send_keys(Keys.END)
+            sleep(1)
+            body.send_keys(Keys.HOME)
+            sleep(1)
+            link_tags = main_tag.find_elements_by_tag_name('a')
+            links_count = len(link_tags)
+            stop = (starting_count == links_count)
+            if stop:
+                break
 
     links = [link.get_attribute('href') for link in link_tags]
 
@@ -48,8 +49,6 @@ def get_links_by_tag(browser, tag, count):
 
 
 def like_post(browser):
-    browser.get('https://www.instagram.com/p/BEVc30BPtBC/')
-
     try:
         like_button = browser.find_elements_by_xpath("//a[@role = 'button']/span[text()='Like']")
     except:
@@ -62,10 +61,13 @@ def like_post(browser):
     if like_button:
         action = ActionChains(browser).move_to_element(like_button[0]).click().perform()
         print('Image liked')
+        sleep(1)
         return True
     elif unlike_button:
         print('Image already liked')
+        sleep(1)
         return False
     else:
         print('Cannot find like button')
+        sleep(1)
         return False
